354ade8ac58565538a4a79cc5d59b1ac74b7b876
mehdi iklef <contact@mehdii.com>
2018-11-13 22:38:20 +0100

    main componentes created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,24 @@ import { RouterModule } from '@angular/router';
 
 import { routes } from './app.routes';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductComponent } from './product/product.component';
+import { CartComponent } from './cart/cart.component';
+import { SearchComponent } from './search/search.component';
+import { OrdersComponent } from './orders/orders.component';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CategoryComponent,
+    ProductComponent,
+    CartComponent,
+    SearchComponent,
+    OrdersComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule, 
